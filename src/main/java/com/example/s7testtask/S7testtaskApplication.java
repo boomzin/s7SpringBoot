44f86e76d5be8cd6c962597d2eafcfad5f8bb4c8
boomzin windows work <boomzin@gmail.com>
2022-02-02 13:36:37 +0300

    init commit

---
+++ src/main/java/com/example/s7testtask/S7testtaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.s7testtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S7testtaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S7testtaskApplication.class, args);
+    }
+
+}
