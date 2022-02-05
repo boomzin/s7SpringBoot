b6fffd7b0bca9435915e3924bde86c6c1a255b48
boomzin windows work <boomzin@gmail.com>
2022-02-05 18:35:49 +0300

    change id generation method

--- src/main/resources/database/populateDB.sql
+++ src/main/resources/database/populateDB.sql
@@ -8,21 +8,21 @@ VALUES ('user1@gmail.com', '{noop}user1pass', 'user1', 16, TRUE, 'MALE', 'OPEN')
        ('user7@gmail.com', '{noop}user7pass', 'user7', 46, TRUE, 'MALE', 'TAKEN');
 
 INSERT INTO user_roles (role, user_id)
-VALUES ('USER', 100000),
-       ('USER', 100001),
-       ('USER', 100002),
-       ('USER', 100003),
-       ('USER', 100004),
-       ('USER', 100005),
-       ('USER', 100006);
+VALUES ('USER', 1),
+       ('USER', 2),
+       ('USER', 3),
+       ('USER', 4),
+       ('USER', 5),
+       ('USER', 6),
+       ('USER', 7);
 
 INSERT INTO FRIENDSHIP (user_id, friend_id)
-VALUES (100000, 100001),
-       (100000, 100002),
-       (100000, 100006),
-       (100001, 100006),
-       (100001, 100005),
-       (100005, 100006),
-       (100005, 100004);
+VALUES (1, 2),
+       (1, 3),
+       (1, 7),
+       (2, 7),
+       (2, 6),
+       (6, 7),
+       (6, 5);
 
 
